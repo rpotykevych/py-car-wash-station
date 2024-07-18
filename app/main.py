e10374860de96cd565482c36b471cc0ff47a2f48
Roman Potykevych <rutkoroman@gmail.com>
2024-07-18 09:25:57 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,18 +1,24 @@
 class Car:
-    def __init__(self, comfort_class: int, clean_mark: int, brand: str) -> None:
+    def __init__(self,
+                 comfort_class: int,
+                 clean_mark: int,
+                 brand: str) -> None:
         self.comfort_class = comfort_class
         self.clean_mark = clean_mark
         self.brand = brand
-    
 
 
 class CarWashStation:
-    def __init__(self, distance_from_city_center: float, clean_power: int, average_rating: float, count_of_ratings: int) -> None:
+    def __init__(self,
+                 distance_from_city_center: float,
+                 clean_power: int,
+                 average_rating: float,
+                 count_of_ratings: int) -> None:
         self.distance_from_city_center = distance_from_city_center
         self.clean_power = clean_power
         self.average_rating = average_rating
-        self.count_of_rating = count_of_ratings
-    
+        self.count_of_ratings = count_of_ratings
+
     def serve_cars(self, cars: list[Car]) -> float:
         income = 0.0
         for car in cars:
@@ -32,7 +38,7 @@ class CarWashStation:
         car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
-        self.count_of_rating += 1
-        average_rating = (self.average_rating + rate) / self.count_of_rating
+        sum_of_rate = self.count_of_ratings * self.average_rating + rate
+        self.count_of_ratings += 1
+        average_rating = sum_of_rate / self.count_of_ratings
         self.average_rating = round(average_rating, 1)
-        
