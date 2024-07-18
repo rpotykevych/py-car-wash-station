d1348e6b9dcc96e699b79b4dfdce8c98d496ebf2
Roman Potykevych <rutkoroman@gmail.com>
2024-07-18 10:47:52 +0300

    add a clean power check for the wash_single_car func

--- app/main.py
+++ app/main.py
@@ -35,7 +35,8 @@ class CarWashStation:
         return round(price, 1)
 
     def wash_single_car(self, car: Car) -> None:
-        car.clean_mark = self.clean_power
+        if car.clean_mark < self.clean_power:
+            car.clean_mark = self.clean_power
 
     def rate_service(self, rate: int) -> None:
         sum_of_rate = self.count_of_ratings * self.average_rating + rate
